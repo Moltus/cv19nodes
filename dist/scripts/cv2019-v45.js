6c321a5d1df4564faa577900a5ef81e95ed4a28d
Moltus <moltus@protonmail.com>
2019-05-14 15:38:10 +0200

    edited cv2019-v45 with react and vuejs entries

---
+++ dist/scripts/cv2019-v45.js
@@ -0,0 +1,511 @@
+const cv2019v4Layout = [
+  {
+    type: "badge",
+    id: "crea-badge",
+    posXY: [
+      164,
+      561
+    ],
+    text: " ------ CREA ------ CREA",
+    color: "#842d50",
+    image: null,
+    children: [
+      "crea1",
+      "crea2"
+    ]
+  },
+  {
+    type: "badge",
+    id: "code-badge",
+    posXY: [
+      749,
+      561
+    ],
+    text: " ------ CODE ------ CODE",
+    color: "#94763c",
+    image: null,
+    children: [
+      "crea-badge",
+      "comm-badge",
+      "code1",
+      "code2",
+      "code3",
+      "code4",
+      "code5",
+      "code6"
+    ]
+  },
+  {
+    type: "badge",
+    id: "comm-badge",
+    posXY: [
+      1360,
+      561
+    ],
+    text: "---- COMM ----- COMM ",
+    color: "#2c5b59",
+    image: null,
+    children: [
+      "comm1",
+      "comm4"
+    ]
+  },
+  {
+    type: "node",
+    id: "crea1",
+    posXY: [
+      81,
+      243
+    ],
+    text: [
+      ">INFOGRAPHISTE 2D/3D",
+      "Freelance 2005-2007, 2011-2018. Multimédia / Print.",
+      "3D : réalisation de vidéos d'animation. Modélisation en box-modeling hi-poly / low-poly. Animation en keyframing.",
+      "2D / print : création d'affiches, flyers, brochures... Utilisation des outils de la gamme Adobe tant pour du matriciel que du vectoriel."
+    ],
+    color: "#d39cb5",
+    image: null,
+    children: [
+      "max3ds",
+      "photoshop",
+      "illustrator"
+    ]
+  },
+  {
+    type: "node",
+    id: "crea2",
+    posXY: [
+      83,
+      696
+    ],
+    text: [
+      "SUPINFOCOM ARLES",
+      "Aujourd'hui Ecole MOPA - Ecole du film d'animation et de l'image de synthèse. 2003-2004.",
+      "E-ART SUP PARIS",
+      "École de multimédia - création numérique. 2000-2003."
+    ],
+    color: "#d39cb5",
+    image: null,
+    children: [
+      "crea3"
+    ]
+  },
+  {
+    type: "node",
+    id: "crea3",
+    posXY: [
+      83,
+      966
+    ],
+    text: [
+      "• Curiosité • Créativité • Polyvalence"
+    ],
+    color: "#d39cb5",
+    image: null,
+    children: []
+  },
+  {
+    type: "node",
+    id: "code1",
+    posXY: [
+      842,
+      669
+    ],
+    text: [
+      "IFOCOP DÉVELOPPEUR WEB",
+      "Montigny-le-Bretonneux 2018-2019. Formation diplomante niveau II.",
+      "Front-end + back-end. Responsive-design, référencement, outils productivité, modélisation bases de données, outils CMS. E-commerce. Création de thèmes Wordpress. "
+    ],
+    color: "#e5cfaa",
+    image: null,
+    children: [
+      "wordpress",
+      "drupal"
+    ]
+  },
+  {
+    type: "node",
+    id: "code2",
+    posXY: [
+      495,
+      669
+    ],
+    text: [
+      "FORMATIONS UDEMY.COM",
+      "\t“Web developper, Mastering the Modern Workflow”, Brad Schiff. “The Complete JavaScript Course”, Jonas Schmedtmann.",
+      "LIVRES PROGRAMMATION",
+      "“Learning Python The Hard Way”, Zed Shaw. “Eloquent Javascript”, Marijn Haverbeke."
+    ],
+    color: "#e5cfaa",
+    image: null,
+    children: [
+      "sass",
+      "gulp",
+      "git"
+    ]
+  },
+  {
+    type: "node",
+    id: "code3",
+    posXY: [
+      495,
+      243
+    ],
+    text: [
+      ">HOTEL PARIMIS",
+      "Création d'un site vitrine front-end 100% responsive pour un hôtel de luxe. HTML5, SCSS, JS, Gulp, Git."
+    ],
+    color: "#e5cfaa",
+    image: null,
+    children: [
+      "html5",
+      "css3",
+      "js"
+    ]
+  },
+  {
+    type: "node",
+    id: "code4",
+    posXY: [
+      840,
+      243
+    ],
+    text: [
+      ">VEVILLE LOCATIONS",
+      "Site applicatif avec backoffice pour une agence de location de véhicules. HTML5, Bootstrap, PHP, MySQL, Ajax, Git."
+    ],
+    color: "#e5cfaa",
+    image: null,
+    children: [
+      "sql",
+      "php",
+      "ajax"
+    ]
+  },
+  {
+    type: "node",
+    id: "code5",
+    posXY: [
+      494,
+      407
+    ],
+    text: [
+      "Réalisation d'un formulaire en Single Page 100% modulaire et responsive avec React JS.",
+      "Création application SPA type dashboard. Vue.js + Vuetify"
+    ],
+    color: "#e5cfaa",
+    image: null,
+    children: ["react", "vuejs"]
+  },
+  {
+    type: "node",
+    id: "code6",
+    posXY: [
+      840,
+      435
+    ],
+    text: [
+      "Programme \"bot\"/solutionneur pour puzzle-game de type \"match\xa03\" (ex. Candy Crush). Python, OpenCV, Numpy."
+    ],
+    color: "#e5cfaa",
+    image: null,
+    children: [
+      "python",
+      "opencv"
+    ]
+  },
+  {
+    type: "node",
+    id: "comm1",
+    posXY: [
+      1279,
+      303
+    ],
+    text: [
+      "PIÈCE THÉÂTRE “ZOBAR”",
+      "Comédien. mise-en-scène M. Tracke. Théâtre de Ménilmontant Paris, 2013-2014.",
+      "COURTS-MÉTRAGES",
+      "Acteur au sein de différents courts-métrages. 2010-2012"
+    ],
+    color: "#9bd4d1",
+    image: null,
+    children: [
+      "comm2"
+    ]
+  },
+  {
+    type: "node",
+    id: "comm2",
+    posXY: [
+      1279,
+      182
+    ],
+    text: [
+      "• Expression en public • Improvisation"
+    ],
+    color: "#9bd4d1",
+    image: null,
+    children: []
+  },
+  {
+    type: "node",
+    id: "comm3",
+    posXY: [
+      609,
+      23
+    ],
+    text: [
+      ">ANGLAIS COURANT"
+    ],
+    color: "#ffffff",
+    image: null,
+    children: []
+  },
+  {
+    type: "node",
+    id: "comm4",
+    posXY: [
+      1279,
+      698
+    ],
+    text: [
+      "COURS FLORENT",
+      "Formation à l'acteur. Jeu face caméra, improvisation, corps en mouvement. Paris 2007-2010."
+    ],
+    color: "#9bd4d1",
+    image: null,
+    children: [
+      "comm5"
+    ]
+  },
+  {
+    type: "node",
+    id: "comm5",
+    posXY: [
+      1279,
+      868
+    ],
+    text: [
+      "• Communication • Adaptabilité • Patience • Structure"
+    ],
+    color: "#9bd4d1",
+    image: null,
+    children: []
+  },
+  {
+    type: "node",
+    id: "max3ds",
+    posXY: [
+      70,
+      114
+    ],
+    text: [],
+    color: "#842d50",
+    image: "images/3dsmax-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "photoshop",
+    posXY: [
+      170,
+      111
+    ],
+    text: [],
+    color: "#842d50",
+    image: "images/photoshop-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "illustrator",
+    posXY: [
+      270,
+      111
+    ],
+    text: [],
+    color: "#842d50",
+    image: "images/illustrator-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "html5",
+    posXY: [
+      485,
+      112
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/html5-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "css3",
+    posXY: [
+      585,
+      111
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/css3-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "js",
+    posXY: [
+      685,
+      110
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/js-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "gulp",
+    posXY: [
+      485,
+      974
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/gulp-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "sass",
+    posXY: [
+      585,
+      974
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/sass-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "sql",
+    posXY: [
+      830,
+      111
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/sql-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "php",
+    posXY: [
+      930,
+      111
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/php-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "ajax",
+    posXY: [
+      1030,
+      114
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/ajax-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "git",
+    posXY: [
+      685,
+      974
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/git-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "wordpress",
+    posXY: [
+      874,
+      974
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/wordpress-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "drupal",
+    posXY: [
+      989,
+      974
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/drupal-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "react",
+    posXY: [
+      384,
+      397
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/react-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "vuejs",
+    posXY: [
+      384,
+      490
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/vuejs-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "python",
+    posXY: [
+      1135,
+      396
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/python-white.svg",
+    children: []
+  },
+  {
+    type: "node",
+    id: "opencv",
+    posXY: [
+      1135,
+      496
+    ],
+    text: [],
+    color: "#94763c",
+    image: "images/opencv-white.svg",
+    children: []
+  }
+]
